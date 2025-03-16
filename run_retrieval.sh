75e24a31c2a748488863e60afe885f914acdd389
Zhiwen You <60921570+zhiwenyou103@users.noreply.github.com>
2025-03-16 16:37:52 -0500

    Update run_retrieval.sh

--- run_retrieval.sh
+++ run_retrieval.sh
@@ -1,5 +1,5 @@
-CONFIG_PATH="label_mappings/s2orc_label_mappings.json"
-OUTPUT_PATH="s2orc_plain_label_words.json"
+CONFIG_PATH="label_mappings/S2ORC_label_mappings.json" # change the path
+OUTPUT_PATH="knowledge_output/s2orc_plain_label_words.json" # change the corresponding output path 
 
 python retrieval.py \
     --config_path "${CONFIG_PATH}" \
