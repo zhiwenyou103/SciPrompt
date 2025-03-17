fe7ccbaf9d91afec4aa0bfc02161309446dc7533
Zhiwen You <60921570+zhiwenyou103@users.noreply.github.com>
2025-03-16 21:23:14 -0500

    Update run_s2orc.sh

--- run_s2orc.sh
+++ run_s2orc.sh
@@ -1,15 +1,16 @@
 python s2orc_script.py \
     --seed 144 \
-    --shots 5 \
+    --shots 5 \ # training shots
     --calibration True \
     --max_seq_length 256 \
     --batch_size 5 \
     --max_epochs 5 \
     --cuda_device 0 \
     --learning_rate 3e-5 \
-    --data_dir "/path/to/your/data/" \
-    --verbalizer_path "/path/to/your/s2orc_knowledgable_verbalizer.txt" \
-    --semantic_score_path "/path/to/your/s2orc_knowledgable_verbalizer_semantic_search_scores.txt" \
-    --doc_id_path "/path/to/your/label_dict.txt" \
+    --soft_verbalizer False \ # whether using the Soft verbalization method 
+    --data_dir "/path/to/your/data/" \ # e.g., data/S2ORC
+    --verbalizer_path "knowledge_output/S2ORC_output_words.txt" \ # filtered verbalizer
+    --semantic_score_path "scores/S2ORC_output_scores.tx" \
+    --doc_id_path "data/S2ORC/label_dict.txt" \
     --config_path "label_mappings/S2ORC_label_mappings.json" \
     --zero_shot no
