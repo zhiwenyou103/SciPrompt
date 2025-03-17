822c3011c3bdb38fc37640f9fc3f580ce583919c
Zhiwen You <60921570+zhiwenyou103@users.noreply.github.com>
2025-03-16 22:03:11 -0500

    Delete OpenPrompt directory

--- OpenPrompt/openprompt/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "1.0.1"
-from .pipeline_base import PromptDataLoader, PromptModel, PromptForClassification
-from .utils import *
-from .prompt_base import Template, Verbalizer
-from .trainer import ClassificationRunner, GenerationRunner
